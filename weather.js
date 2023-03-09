bde2474531079a884742944c6b56eb3be2d02d4c
Sumit <104670798+P0SSIBLE-0@users.noreply.github.com>
2023-03-09 17:54:56 +0530

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,12 +1,12 @@
-
-async function getWeather(lat = 28.65, long= 77.23, timezone) {
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,weathercode,surface_pressure,visibility,windspeed_10m&current_weather=true&precipitation_unit=inch&timeformat=unixtime`
-    await fetch(url).then((res) => res.json())
-        .then(data => updateWeather(data))
-        .catch(err => console.log(err));
-}
 let timeStamp = Intl.DateTimeFormat().resolvedOptions().timeZone
 console.log(timeStamp)
+
+function getWeather(lat = 28.65, long = 77.23) {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,weathercode,surface_pressure,visibility,windspeed_10m&current_weather=true&precipitation_unit=inch&timeformat=unixtime`
+  fetch(url).then((res) => res.json())
+    .then(data => updateWeather(data))
+    .catch(err => console.log(err));
+}
 
 let current_temp = document.querySelector('.temp');
 let today_date = document.querySelector('.date');
@@ -18,47 +18,46 @@ let visibility = document.querySelector('[data-visibility]');
 let weatherIcon = document.querySelector('#icon');
 
 const updateWeather = (data) => {
-    current_temp.innerHTML = Math.round(data.current_weather.temperature) + '&deg;C'
-    let unix_time = data.current_weather.time
-    let date = new Date(unix_time * 1000)
-    today_date.innerHTML = date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', weekday: 'short' })
-    apparent_temperature.textContent = `${data.hourly.apparent_temperature[data.hourly.apparent_temperature.length - 1]}°C`
-    humidity.textContent = data.hourly.relativehumidity_2m[data.hourly.relativehumidity_2m.length - 1] + '%'
-    wind.textContent = data.current_weather.windspeed + ' Km/h'
-    airPressure.textContent = Math.round(data.hourly.surface_pressure[data.hourly.surface_pressure.length - 1]) + 'hPa'
-    visibility.textContent = Math.floor((data.hourly.visibility[data.hourly.visibility.length - 1]) / 3280) + ' Km/h'
-    var weathercode = data.current_weather.weathercode
-    changeIcon(weathercode)
+  current_temp.innerHTML = Math.round(data.current_weather.temperature) + '&deg;C'
+  let unix_time = data.current_weather.time
+  let date = new Date(unix_time * 1000)
+  today_date.innerHTML = date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', weekday: 'short' })
+  apparent_temperature.textContent = `${data.hourly.apparent_temperature[data.hourly.apparent_temperature.length - 1]}°C`
+  humidity.textContent = data.hourly.relativehumidity_2m[data.hourly.relativehumidity_2m.length - 1] + '%'
+  wind.textContent = data.current_weather.windspeed + ' Km/h'
+  airPressure.textContent = Math.round(data.hourly.surface_pressure[data.hourly.surface_pressure.length - 1]) + 'hPa'
+  visibility.textContent = Math.floor((data.hourly.visibility[data.hourly.visibility.length - 1]) / 3280) + ' Km/h'
+  var weathercode = data.current_weather.weathercode
+  changeIcon(weathercode)
 }
 if (window.navigator.geolocation) {
-    window.navigator.geolocation.getCurrentPosition(showPosition);
+  window.navigator.geolocation.getCurrentPosition(showPosition);
 } else {
-    console.log("Geolocation is not supported by this browser.");
+  console.log("Geolocation is not supported by this browser.");
 }
 
 function showPosition(position) {
-   let lat =  position.coords.latitude 
-   let long = position.coords.longitude;
-   getWeather(lat,long, Intl.DateTimeFormat().resolvedOptions().timeZone)
+  let lat = position.coords.latitude
+  let long = position.coords.longitude;
+  getWeather(lat, long)
 }
-getWeather(lat,long, Intl.DateTimeFormat().resolvedOptions().timeZone)
 
 const changeIcon = (weathercode) => {
-    if (weathercode === 0) {
-        weatherIcon.src = 'sun.png'
-    } else if (weathercode <= 3) {
-        weatherIcon.src = 'cloudy.png'
+  if (weathercode === 0) {
+    weatherIcon.src = 'icons/sun.png'
+  } else if (weathercode <= 3) {
+    weatherIcon.src = 'icons/cloudy.png'
 
-    } else if (weathercode === 61 || weathercode === 63 || weathercode === 65 || weathercode === 66 || 67) {
-        weatherIcon.src = 'cloudy (1).png'
-    } else if (weathercode === 71 || weathercode === 73 || weathercode === 75) {
+  } else if (weathercode === 61 || weathercode === 63 || weathercode === 65 || weathercode === 66 || 67) {
+    weatherIcon.src = 'icons/cloudy (1).png'
+  } else if (weathercode === 71 || weathercode === 73 || weathercode === 75) {
 
-    } else if (weathercode > 80) {
-        weatherIcon.src = 'rain.png'
-    } else {
-        weatherIcon.src = 'storm.png'
-    }
+  } else if (weathercode > 80) {
+    weatherIcon.src = 'icons/rain.png'
+  } else {
+    weatherIcon.src = 'icons/storm.png'
+  }
 }
 window.addEventListener('load', () => {
-    document.querySelector('.container').classList.remove("blur");
+  document.querySelector('.container').classList.remove("blur");
 })
